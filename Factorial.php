71d65a3fd6a3fe0288410c6a968be5421dc54100
Guié Y. Miranda Contreras <60778511+Guie-Miranda@users.noreply.github.com>
2022-08-02 15:09:23 -0500

    Ejercicio 1
    Fecha 02/08/2022

---
+++ Factorial.php
@@ -0,0 +1,27 @@
+<html> <!-- Aquí se inicia ek html -->
+        <head>
+                <title>Factorial</title>
+        </head>
+
+        <body>
+            <form method="POST">
+                <label>Ingresa un número entero: </label>
+                <input type="text" name="entrada">
+                <br> <!-- Este es el salto de linea -->
+                <button type="submit">Calcular</button>
+                <br>
+                <label>El resultado es: </label>
+            </form>
+        </body>
+</html>
+<?php //es la etiqueta de apertura
+    $m = $_POST['entrada'];
+    function factorial($n){
+        if($n == 0 || $n == 1)
+            return 1; 
+        else   
+            return $n*factorial($n-1);
+    }
+    echo factorial($m);
+//el de abajo es la etiqueta de cierre, es importante ponerla su hay otras funciones que no sean PHP
+?>
